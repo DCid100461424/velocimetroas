fe038c2db616c3ab616c707f4e68c3be93c0f25c
DCid100461424 <100461424@alumnos.uc3m.es>
2025-05-25 14:13:32 +0200

    Refactorizado un poco el código del constructor. Añadida función toggleTraining.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:provider/provider.dart';
@@ -5,6 +7,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:status_alert/status_alert.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -54,6 +57,10 @@ class _SpeedometerScreenState extends State<SpeedometerScreen> {
   StreamSubscription<BluetoothConnectionState>? deviceStateSubscription;
   BluetoothCharacteristic? writeCharacteristic;
   StreamSubscription<List<int>>? characteristicSubscription;
+  //StreamSubscription<List<int>>? timeSubscription;
+  StreamSubscription<List<int>>? avgSpeedSubscription;
+  StreamSubscription<List<int>>? distanceSubscription;
+  StreamSubscription<List<int>>? temperatureSubscription;
 
   // App state variables
   final int DISCONNECTED_STATE = 0;
@@ -64,14 +71,21 @@ class _SpeedometerScreenState extends State<SpeedometerScreen> {
   double avgSpeed = 0.0; // Velocidad media en km/h
   double totalDistance = 0.0; // Distancia total recorrida en km/h
   double currentTemp = 0.0; // Temperatura actual en Celsius
+  Stopwatch chrono = Stopwatch(); // Cronómetro
+
   bool isConnecting = false;
   bool isScanning = false;
   bool isTraining = false;
 
   // Define UUIDs for the service and characteristics
-  final String serviceUuid = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-  final String writeCharacteristicUuid = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
-  final String notifyCharacteristicUuid = "cba1d466-344c-4be3-ab3f-189f80d751a2";
+  // Posible nueva: dc9a393d-bebf-4d11-8235-01878bbec7fe
+  final String SERVICE_UUID        = "4fafc201-0000-459e-8fcc-c5c9c331914b";
+  final String WRITE_CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+  final String NOTIFY_CHARACTERISTIC_UUID = "cba1d466-344c-4be3-ab3f-189f80d751a2";
+  final String AVG_SPEED_UUID="4fafc201-0001-459e-8fcc-c5c9c331914b";
+  final String TOTAL_DISTANCE_UUID="4fafc201-0002-459e-8fcc-c5c9c331914b";
+  final String TEMPERATURE_UUID="4fafc201-0003-459e-8fcc-c5c9c331914b";
+  final String TOTAL_TIME_UUID="4fafc201-0004-459e-8fcc-c5c9c331914b";
 
   @override
   Widget build(BuildContext context) {
@@ -99,83 +113,6 @@ class _SpeedometerScreenState extends State<SpeedometerScreen> {
       ),
     );
   }
-
-  Expanded buildMeasureBox() {
-    return Expanded(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Caja de Velocidad
-          Expanded(
-            child:buildMeasurementCard("Velocidad", currentSpeed.toStringAsFixed(1), 'km/h', isTraining, true),
-          ),
-
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              buildMeasurementCard("Veloc. media", currentSpeed.toStringAsFixed(1), 'km/h', isTraining, false),
-              buildMeasurementCard("Distancia total", currentSpeed.toStringAsFixed(1), 'km', isTraining, false),
-            ],
-          ),
-
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              buildMeasurementCard("Temperatura", currentSpeed.toStringAsFixed(1), 'ºC', isTraining, false),
-              buildMeasurementCard("Tiempo entren.", currentSpeed.toStringAsFixed(1), 'h', isTraining, false),
-            ],
-          ),
-
-        ])
-
-    );
-  }
-
-  // Un método para construir una Card de medidas, como la de velocidad.
-  // Si está conectado, saldrá azul. Si no, saldrá gris.
-  Card buildMeasurementCard(String measureName, String measureValue, String measureUnits, bool isActive, bool bigFont) {
-    return Card(
-            color: isActive ? Colors.blue : Colors.grey[300],
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        measureValue,
-                        style: TextStyle(
-                          fontSize: bigFont? 72.0 : 50.0,
-                          fontWeight: FontWeight.bold,
-                          color: isActive ? Colors.white : Colors.grey[600],
-                        ),
-                      ),
-                      const SizedBox(width:10),
-                      Text(
-                        measureUnits,
-                        style: TextStyle(
-                          fontSize: bigFont? 24.0: 18.0,
-                          color: isActive ? Colors.white : Colors.grey[600],
-                        ),
-                      ),
-                      ]),
-
-                  const SizedBox(width:10),
-
-                  Text(
-                    measureName,
-                    style: TextStyle(
-                      fontSize: bigFont? 24.0: 18.0,
-                      color: isActive ? Colors.white : Colors.grey[600],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          );
-  }
-
 
   Card buildConnectBox() {
     return Card(
@@ -235,6 +172,141 @@ class _SpeedometerScreenState extends State<SpeedometerScreen> {
         ),
       ),
     );
+  }
+
+  Expanded buildMeasureBox() {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Caja de Velocidad
+          Expanded(
+            child:buildMeasurementCard("Velocidad", currentSpeed.toStringAsFixed(1), 'km/h', isTraining, true),
+          ),
+
+
+          // Cajas de medidas adicionales
+          Table(
+            columnWidths: const {
+              0: FlexColumnWidth(),
+              1: FlexColumnWidth(),
+            },
+            children: [
+            TableRow(
+              children: [
+                buildMeasurementCard("Veloc. media", avgSpeed.toStringAsFixed(1), 'km/h', isTraining, false),
+                buildMeasurementCard("Distancia total", totalDistance.toStringAsFixed(1), 'km', isTraining, false),
+              ],
+            ),
+
+            TableRow(
+              children: [
+                buildMeasurementCard("Temperatura", currentTemp.toStringAsFixed(1), 'ºC', isTraining, false),
+                buildMeasurementCard("Tiempo entren.", chrono.elapsed.toString().substring(0,7), 'h', isTraining, false),
+              ],
+            )],
+          ),
+
+          //TODO: Botón de entrenamiento
+          //Button
+          // icon: isTraining ? icon.pause : icon.start
+          // (ver el título del icono, y ver para poner la caja en gris cuando no esté conectado)
+          // onPressed: (método. pedir confirmación para parar o iniciar?)
+          ElevatedButton(
+            onPressed: () {
+              toggleTraining();
+            },
+            //TODO: Ver si hay que modificar el estilo
+            style: ElevatedButton.styleFrom(
+                backgroundColor:isTraining? Colors.redAccent:  connectedDevice==null? Colors.grey[400]: Colors.green[200],
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                textStyle: const TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold)),
+            child: Column(
+              children:[
+                isTraining? const Icon(Icons.stop): const Icon(Icons.play_arrow),
+                isTraining? const Text('Terminar entrenamiento'): const Text('Empezar entrenamiento'),
+              ]
+            ),
+          ),
+
+          // TODO: Botón extra para reiniciar?? o que no se pause y en vez de eso se reinicie?
+        ])
+
+    );
+  }
+
+  // Un método para construir una Card de medidas, como la de velocidad.
+  // Si está conectado, saldrá azul. Si no, saldrá gris.
+  Card buildMeasurementCard(String measureName, String measureValue, String measureUnits, bool isActive, bool bigFont) {
+    return Card(
+            color: isActive ? Colors.blue : Colors.grey[300],
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        measureValue,
+                        style: TextStyle(
+                          fontSize: bigFont? 72.0 : 40.0,
+                          fontWeight: bigFont? FontWeight.bold: FontWeight.normal,
+                          color: isActive ? Colors.white : Colors.grey[600],
+                        ),
+                      ),
+                      const SizedBox(width:10),
+                      Text(
+                        measureUnits,
+                        style: TextStyle(
+                          fontSize: bigFont? 24.0: 18.0,
+                          color: isActive ? Colors.white : Colors.grey[600],
+                        ),
+                      ),
+                      ]),
+
+                  const SizedBox(width:10),
+
+                  Text(
+                    measureName,
+                    style: TextStyle(
+                      fontSize: bigFont? 24.0: 18.0,
+                      color: isActive ? Colors.white : Colors.grey[600],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+  }
+
+  // TODO: Hacer una función real
+  Future<void> toggleTraining() async {
+    if (connectedDevice != null && writeCharacteristic != null) {
+      try {
+        if(isTraining){
+          // Para parar mandamos STOP y que haga una comprobación en el código. Si es STOP para, si es un valor empieza.
+          List<int> bytes = utf8.encode("STOP");
+          await writeCharacteristic!.write(bytes);
+
+          isTraining = false;
+          chrono.stop();
+        }
+        else {
+          List<int> bytes = utf8.encode(wheelDiameter.toString());
+          await writeCharacteristic!.write(bytes);
+
+          isTraining = true;
+          chrono.start();
+        }
+      } catch (e) {
+        showStatusAlert(context, 'Error mandando señal al microcontrolador: $e');
+      }
+    }
+
+
   }
 
 
@@ -438,24 +510,23 @@ class _SpeedometerScreenState extends State<SpeedometerScreen> {
       // Find the appropriate service and characteristics
       for (BluetoothService service in services) {
         // Check if this is the service we're looking for
-        if (service.uuid.toString() == serviceUuid) {
+        if (service.uuid.toString() == SERVICE_UUID) {
           // Find the write characteristic
           for (BluetoothCharacteristic characteristic in service.characteristics) {
             // Check if this is the write characteristic
-            if (characteristic.uuid.toString() == writeCharacteristicUuid) {
+            if (characteristic.uuid.toString() == WRITE_CHARACTERISTIC_UUID) {
               writeCharacteristic = characteristic;
               foundWrite = true;
             }
 
             // Check if this is the notify characteristic
-            if (characteristic.uuid.toString() == notifyCharacteristicUuid) {
+            if (characteristic.uuid.toString() == NOTIFY_CHARACTERISTIC_UUID) {
               foundNotify = true;
               // Set up notification
               await characteristic.setNotifyValue(true);
 
               // Listen for notifications (speed data)
-              characteristicSubscription = characteristic.onValueReceived.listen(
-                      (value) {
+              characteristicSubscription = characteristic.onValueReceived.listen((value) {
                     if (value.isNotEmpty) {
                       // Parse the speed data from the ESP32
                       try {
@@ -474,7 +545,86 @@ class _SpeedometerScreenState extends State<SpeedometerScreen> {
                   }
               );
             }
-          }
+
+            // Check if this is the notify characteristic
+            if (characteristic.uuid.toString() == AVG_SPEED_UUID) {
+              // Set up notification
+              await characteristic.setNotifyValue(true);
+
+              // Listen for notifications (speed data)
+              characteristicSubscription = characteristic.onValueReceived.listen((value) {
+                if (value.isNotEmpty) {
+                  // Parse the speed data from the ESP32
+                  try {
+                    final avgspeedString = String.fromCharCodes(value);
+                    final avgspeedValue = double.parse(avgspeedString);
+                    setState(() {
+                      avgSpeed = avgspeedValue;
+                    });
+                  } catch (e) {
+                    showStatusAlert(context, "Error parsing speed data: $e");
+                  }
+                }
+              },
+                  onError: (e) {
+                    showStatusAlert(context, "Characteristic notification error: $e");
+                  }
+              );
+            }
+
+            // Check if this is the notify characteristic
+            if (characteristic.uuid.toString() == TOTAL_DISTANCE_UUID) {
+              // Set up notification
+              await characteristic.setNotifyValue(true);
+
+              // Listen for notifications (speed data)
+              distanceSubscription = characteristic.onValueReceived.listen((value) {
+                if (value.isNotEmpty) {
+                  // Parse the speed data from the ESP32
+                  try {
+                    final distanceString = String.fromCharCodes(value);
+                    final distanceValue = double.parse(distanceString);
+                    setState(() {
+                      totalDistance = distanceValue;
+                    });
+                  } catch (e) {
+                    showStatusAlert(context, "Error parsing distance data: $e");
+                  }
+                }
+              },
+                  onError: (e) {
+                    showStatusAlert(context, "Characteristic notification error: $e");
+                  }
+              );
+            }
+
+            // Check if this is the temperature characteristic
+            if (characteristic.uuid.toString() == TEMPERATURE_UUID) {
+              // Set up notification
+              await characteristic.setNotifyValue(true);
+
+              // Listen for notifications (speed data)
+              temperatureSubscription = characteristic.onValueReceived.listen((value) {
+                if (value.isNotEmpty) {
+                  // Parse the speed data from the ESP32
+                  try {
+                    final tempString = String.fromCharCodes(value);
+                    final tempValue = double.parse(tempString);
+                    setState(() {
+                      currentTemp = tempValue;
+                    });
+                  } catch (e) {
+                    showStatusAlert(context, "Error parsing temperature data: $e");
+                  }
+                }
+              },
+                  onError: (e) {
+                    showStatusAlert(context, "Characteristic notification error: $e");
+                  }
+              );
+            }
+
+            }
         }
       }
 
